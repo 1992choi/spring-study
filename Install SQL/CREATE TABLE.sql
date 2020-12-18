a736e4b175e253f013de09bea70ec96a07f93543
choi <younggeunn@naver.com>
2020-12-18 18:23:56 +0900

    DB 설치 스크립트 추가

---
+++ Install SQL/CREATE TABLE.sql
@@ -0,0 +1,7 @@
+CREATE TABLE member 
+( 
+    member_id       VARCHAR2(6)	NOT NULL,
+    member_name     VARCHAR2(100),
+    phone_number    VARCHAR2(20),
+    email           VARCHAR2(100)
+);
---
+++ Install SQL/INSERT DATA.sql
@@ -0,0 +1,4 @@
+INSERT INTO member 
+VALUES ('000001', 'CHOI', '010-4884-8084', 'younggeunn@naver.com');
+
+COMMIT;
